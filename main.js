689acb38d2212bd154c4345c2126c3322a8a31ba
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-07-02 14:49:00 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let coin = {
+    state: 0,
+    // *1. Randomly set your coi object's "state" property to be either 0 or 1: "this.state" to access the "state" property on this object. */
+    flip: function() {
+
+
+    },
+
+    // *2. Return the string "Heads" or "Tails" depending on weather "this.state" is a 0 or 1. */
+    toString: function() {
+
+    },
+
+
+    // 3. Set the image properties of this element to show either f or face-down, depending on weather this state is 0 or 1.*/
+        toHTML: function() {
+            let image = document.createElement('img');
+
+return image;
+        }
+    };
+
